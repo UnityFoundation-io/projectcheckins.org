e74a52b23a78a59936a98bd75db4b3b8083effa6
Sergio del Amo <sergio.delamo@softamo.com>
2024-02-29 11:54:30 +0100

    Apply suggestions from code review
    Co-authored-by: Guillermo Calvo <guillermocalvo@gmail.com>

--- multitenancy/src/main/java/io/micronaut/multitenancy/Tenant.java
+++ multitenancy/src/main/java/io/micronaut/multitenancy/Tenant.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 /**
  * Represents a tenant.
  * @author Sergio del Amo
- * @since 5.3.0
  */
 public record Tenant(@NotBlank String id) {
 }
--- multitenancy/src/main/java/io/micronaut/multitenancy/http/MultitenancyFilter.java
+++ multitenancy/src/main/java/io/micronaut/multitenancy/http/MultitenancyFilter.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2017-2024 original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.micronaut.multitenancy.http;
 
 import io.micronaut.context.annotation.Requires;
@@ -31,7 +16,6 @@ import java.io.Serializable;
 /**
  * Populates the current tenant, if resolved, in the request attribute {@value #TENANT}.
  * @author Sergio del Amo
- * @since 5.3.0
  */
 @Requires(property = MultitenancyFilterConfigurationProperties.PREFIX + ".enabled", notEquals = StringUtils.FALSE, defaultValue = StringUtils.TRUE)
 @Requires(bean = TenantResolver.class)
