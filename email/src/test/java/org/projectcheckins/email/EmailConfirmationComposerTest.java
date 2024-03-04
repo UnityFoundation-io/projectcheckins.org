ab723a4115b37462e104b04baa7c8572c5f8f794
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-04 12:03:31 +0100

    Update email/src/test/java/org/projectcheckins/email/EmailConfirmationComposerTest.java
    Co-authored-by: Guillermo Calvo <guillermocalvo@gmail.com>

--- email/src/test/java/org/projectcheckins/email/EmailConfirmationComposerTest.java
+++ email/src/test/java/org/projectcheckins/email/EmailConfirmationComposerTest.java
@@ -19,23 +19,19 @@ class EmailConfirmationComposerTest {
     void emailConfirmation(EmailConfirmationComposer emailConfirmationComposer) {
         Email.Builder emailBuilder = emailConfirmationComposer.composeEmailConfirmation(Locale.ENGLISH,
                 new EmailConfirmation("https://projectcheckins.example.com/email/confirm", "sergio.delamo@softamo.com"));
-        assertNotNull(emailBuilder);
-        Email email = emailBuilder.build();
-        assertEquals("Confirm email address for your Project Check-ins account", email.getSubject());
-        assertEquals("info@projectcheckins.org", email.getFrom().getEmail());
-        assertEquals(1, email.getTo().size());
-        assertEquals("sergio.delamo@softamo.com", email.getTo().stream().findFirst().get().getEmail());
-        assertTrue(email.getBody() instanceof MultipartBody);
-        MultipartBody multipartBody = (MultipartBody) email.getBody();
-        assertFalse(multipartBody.get(BodyType.TEXT).isEmpty());
-
-        assertTrue(multipartBody.get(BodyType.TEXT).get().startsWith("""
+        assertThat(emailBuilder)
+            .isNotNull()
+            .extracting(Email.Builder::build)
+            .hasFieldOrPropertyWithValue("subject", "Confirm email address for your Project Check-ins account")
+            .hasFieldOrPropertyWithValue("from", new Contact("info@projectcheckins.org"))
+            .hasFieldOrPropertyWithValue("to", List.of(new Contact("sergio.delamo@softamo.com")))
+            .extracting(Email::getBody)
+            .isInstanceOf(MultipartBody.class)
+            .satisfies(b -> assertThat(b.get(BodyType.TEXT)).hasValueSatisfying(x -> assertThat(x).startsWith("""
                 To confirm your email address, please click the link below:
-                https://projectcheckins.example.com/email/confirm?token="""));
-        assertFalse(multipartBody.get(BodyType.HTML).isEmpty());
-        assertTrue(multipartBody.get(BodyType.HTML).get().startsWith("""
+                https://projectcheckins.example.com/email/confirm?token=""")))
+            .satisfies(b -> assertThat(b.get(BodyType.HTML)).hasValueSatisfying(x -> assertThat(x).startsWith("""
                 To confirm your email address, please click the link below:
-                <a href="https://projectcheckins.example.com/email/confirm?token="""));
-
+                <a href="https://projectcheckins.example.com/email/confirm?token=""")));
     }
 }
