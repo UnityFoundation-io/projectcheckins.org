948a8e53b04a7d08e9650f871135ad44e738ee82
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-04 11:57:18 +0100

    Update email-http/src/test/java/org/projectcheckins/email/http/EmailConfirmationControllerConfigurationPropertiesTest.java
    Co-authored-by: Guillermo Calvo <guillermocalvo@gmail.com>

--- email-http/src/test/java/org/projectcheckins/email/http/EmailConfirmationControllerConfigurationPropertiesTest.java
+++ email-http/src/test/java/org/projectcheckins/email/http/EmailConfirmationControllerConfigurationPropertiesTest.java
@@ -2,7 +2,7 @@ package org.projectcheckins.email.http;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class EmailConfirmationControllerConfigurationPropertiesTest {
 
