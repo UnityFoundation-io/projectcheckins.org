b5408ea7bb4e1590df2453a86ad3d50b062d0e31
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-04 12:03:44 +0100

    Update email/src/test/java/org/projectcheckins/email/EmailConfirmationTokenGeneratorTest.java
    Co-authored-by: Guillermo Calvo <guillermocalvo@gmail.com>

--- email/src/test/java/org/projectcheckins/email/EmailConfirmationTokenGeneratorTest.java
+++ email/src/test/java/org/projectcheckins/email/EmailConfirmationTokenGeneratorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @Property(name = "micronaut.security.token.jwt.signatures.secret.generator.secret", value="pleaseChangeThisSecretForANewOne")
