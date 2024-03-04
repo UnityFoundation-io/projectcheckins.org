53f3c32c7e2787399bcef5f1559334c8ed8e6a95
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-04 07:50:41 +0100

    add some javadoc

--- email-http/src/main/java/org/projectcheckins/email/http/EmailConfirmationControllerConfiguration.java
+++ email-http/src/main/java/org/projectcheckins/email/http/EmailConfirmationControllerConfiguration.java
@@ -4,12 +4,21 @@ import io.micronaut.core.annotation.NonNull;
 
 public interface EmailConfirmationControllerConfiguration {
 
+    /**
+     * @return The path to send the user to to confirm its email. Typically, a link in an email.
+     */
     @NonNull
     String getPath();
 
+    /**
+     * @return The path to redirect to after a successfully confirming a user's email
+     */
     @NonNull
     String getSuccessfulRedirect();
 
+    /**
+     * @return The path to redirect to after an unsuccessful confirmation.
+     */
     @NonNull
     String getFailureRedirect();
 }
