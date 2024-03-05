88dffed5cf7c60733ebf829e970195a99e1bef58
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-05 10:37:26 +0100

    markdown renderer
    markdown

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,6 +5,9 @@ dependencies {
     api(project(":multitenancy"))
     api("io.micronaut.security:micronaut-security")
     api("io.micronaut.views:micronaut-views-fieldset")
+
+    implementation("com.vladsch.flexmark:flexmark:${project.properties["flexmarkVersion"]}")
+
     implementation("com.fasterxml.jackson.core:jackson-annotations")
     compileOnly("com.fasterxml.jackson.core:jackson-databind")
     testCompileOnly("com.fasterxml.jackson.core:jackson-databind")
