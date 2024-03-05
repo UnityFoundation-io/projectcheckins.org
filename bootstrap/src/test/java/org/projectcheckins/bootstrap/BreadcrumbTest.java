8befb2e6342333c82f43c82880b6ad3ee7a73a34
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-05 10:37:05 +0100

    add breadcrumb

---
+++ bootstrap/src/test/java/org/projectcheckins/bootstrap/BreadcrumbTest.java
@@ -0,0 +1,22 @@
+package org.projectcheckins.bootstrap;
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.micronaut.views.fields.messages.Message;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.Test;
+import static org.projectcheckins.bootstrap.AssertUtils.*;
+
+@MicronautTest(startApplication = false)
+class BreadcrumbTest {
+
+    @Test
+    void labelNotNull(Validator validator) {
+        assertNotNull(validator, new Breadcrumb(null, null), "label");
+    }
+
+    @Test
+    void validBreadcrumb(Validator validator) {
+        assertValid(validator, new Breadcrumb(Message.of("Hello World")));
+    }
+
+}
