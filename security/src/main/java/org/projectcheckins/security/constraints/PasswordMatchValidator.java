9a2d959bb7521df1d012e8334d70c5c055520277
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-05 06:34:52 +0100

    simplify code

--- security/src/main/java/org/projectcheckins/security/constraints/PasswordMatchValidator.java
+++ security/src/main/java/org/projectcheckins/security/constraints/PasswordMatchValidator.java
@@ -7,14 +7,8 @@ import jakarta.validation.ConstraintValidatorContext;
 @Introspected
 public class PasswordMatchValidator implements ConstraintValidator<PasswordMatch, RepeatPasswordForm> { @Override
     public boolean isValid(RepeatPasswordForm value, ConstraintValidatorContext context) {
-        if (value.password() == null && value.repeatPassword() == null) {
-            return true;
-        }
-        if (value.password() != null && value.repeatPassword() == null) {
-            return false;
-        }
         if (value.password() == null) {
-            return false;
+            return value.repeatPassword() == null;
         }
         return value.password().equals(value.repeatPassword());
     }
