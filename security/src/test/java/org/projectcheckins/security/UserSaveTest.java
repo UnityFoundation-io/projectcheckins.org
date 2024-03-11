d52c73731cbf784d0a0db08f0aa194700be930da
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-11 11:20:20 +0100

    revert changes to test

--- security/src/test/java/org/projectcheckins/security/UserSaveTest.java
+++ security/src/test/java/org/projectcheckins/security/UserSaveTest.java
@@ -1,16 +1,14 @@
 package org.projectcheckins.security;
 
-import static java.util.Collections.emptyList;
-import static java.util.TimeZone.getDefault;
-import static java.time.DayOfWeek.MONDAY;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
-
 import io.micronaut.core.type.Argument;
 import io.micronaut.serde.SerdeIntrospections;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.micronaut.validation.validator.Validator;
 import org.junit.jupiter.api.Test;
+
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 @MicronautTest(startApplication = false)
 class UserSaveTest {
@@ -30,18 +28,6 @@ class UserSaveTest {
             .isNotEmpty();
         assertThat(validator.validate(new UserSave("manolo", "", emptyList())))
             .anyMatch(x -> x.getPropertyPath().toString().equals("encodedPassword") && x.getMessage().equals("must not be blank"));
-    }
-
-    @Test
-    void timeZoneIsRequired(Validator validator) {
-        assertThat(validator.validate(new UserSave("manolo", null, emptyList())))
-            .isNotEmpty();
-    }
-
-    @Test
-    void firstDayOfWeekIsRequired(Validator validator) {
-        assertThat(validator.validate(new UserSave("manolo", null, emptyList())))
-            .isNotEmpty();
     }
 
     @Test
