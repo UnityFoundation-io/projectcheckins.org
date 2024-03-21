e3f1eb30dce89052a56e80d06b5bb2a3b9981455
Guillermo Calvo <guillermo@guillermo.dev>
2024-03-21 10:11:55 +0100

    feat: list profiles

--- core/src/main/java/org/projectcheckins/core/repositories/ProfileRepository.java
+++ core/src/main/java/org/projectcheckins/core/repositories/ProfileRepository.java
@@ -6,6 +6,7 @@ import io.micronaut.multitenancy.Tenant;
 import io.micronaut.security.authentication.Authentication;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 import java.util.Optional;
 import org.projectcheckins.core.api.Profile;
 import org.projectcheckins.core.forms.ProfileUpdate;
@@ -13,9 +14,17 @@ import org.projectcheckins.core.forms.ProfileUpdate;
 public interface ProfileRepository {
 
   @NonNull
+  List<? extends Profile> list(@Nullable Tenant tenant);
+
+  @NonNull
   Optional<? extends Profile> findByAuthentication(@NotNull Authentication authentication, @Nullable Tenant tenant);
 
   void update(@NotNull Authentication authentication, @NotNull @Valid ProfileUpdate profileUpdate, @Nullable Tenant tenant);
+
+  @NonNull
+  default List<? extends Profile> list() {
+      return list(null);
+  }
 
   @NonNull
   default Optional<? extends Profile> findByAuthentication(@NotNull Authentication authentication) {
