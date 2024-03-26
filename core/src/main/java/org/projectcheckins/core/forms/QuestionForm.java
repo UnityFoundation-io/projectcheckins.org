12811e600f6599da9cd1fc295b3fcc777b494b2a
Guillermo Calvo <guillermo@guillermo.dev>
2024-03-26 15:45:58 +0100

    fix: question form - get time of day

--- core/src/main/java/org/projectcheckins/core/forms/QuestionForm.java
+++ core/src/main/java/org/projectcheckins/core/forms/QuestionForm.java
@@ -10,6 +10,8 @@ import java.util.Set;
 
 public interface QuestionForm extends ValidatedFieldset {
     @NotNull HowOften howOften();
+    @NotNull
+    TimeOfDay timeOfDay();
     @Nullable
     Set<DayOfWeek> dailyOnDay();
     @Nullable
