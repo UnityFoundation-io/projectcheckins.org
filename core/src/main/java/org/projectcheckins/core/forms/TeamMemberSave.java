0cd9213dc660e14721b227e5081162ce2f84b569
Guillermo Calvo <guillermo@guillermo.dev>
2024-04-11 11:17:52 +0200

    feat: team service

---
+++ core/src/main/java/org/projectcheckins/core/forms/TeamMemberSave.java
@@ -0,0 +1,9 @@
+package org.projectcheckins.core.forms;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.serde.annotation.Serdeable;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Serdeable
+public record TeamMemberSave(@NonNull @NotBlank @Email String email) { }
