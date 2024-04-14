acfe48a63be6098df2a1c40302480252cc142a91
Sergio del Amo <sergio.delamo@softamo.com>
2024-04-14 11:29:25 +0200

    add nullability annotation to return type

--- security/src/main/java/org/projectcheckins/security/RegistrationCheck.java
+++ security/src/main/java/org/projectcheckins/security/RegistrationCheck.java
@@ -1,9 +1,9 @@
 package org.projectcheckins.security;
 
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.order.Ordered;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-
 import java.util.Optional;
 
 public interface RegistrationCheck extends Ordered {
@@ -13,5 +13,6 @@ public interface RegistrationCheck extends Ordered {
      * @param email The email address to register.
      * @return An empty optional if the registration is possible with the supplied email address.
      */
+    @NonNull
     Optional<RegistrationCheckViolation> validate(@NotBlank @Email String email);
 }
