d50eee2351fa8f9ddac1e74c74f667399156f4d3
Guillermo Calvo <guillermo@guillermo.dev>
2024-04-16 15:40:23 +0200

    feat: Simplify `MapViewModelProcessor`

--- core/src/main/java/org/projectcheckins/core/viewmodelprocessors/MapViewModelProcessor.java
+++ core/src/main/java/org/projectcheckins/core/viewmodelprocessors/MapViewModelProcessor.java
@@ -7,22 +7,21 @@ import io.micronaut.views.model.ViewModelProcessor;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiConsumer;
 
 public abstract class MapViewModelProcessor implements ViewModelProcessor<Map<String, Object>> {
     @Override
     public void process(@NonNull HttpRequest<?> request, @NonNull ModelAndView<Map<String, Object>> modelAndView) {
-        populateModelAndView(request, modelAndView, this::populateModel);
+        populateModelAndView(request, modelAndView);
     }
 
-    private void populateModelAndView(@NonNull HttpRequest<?> request, ModelAndView<Map<String, Object>> modelAndView, BiConsumer<HttpRequest<?>, Map<String, Object>> viewModelConsumer) {
+    private void populateModelAndView(@NonNull HttpRequest<?> request, ModelAndView<Map<String, Object>> modelAndView) {
         Map<String, Object> viewModel = modelAndView.getModel().orElseGet(() -> {
             final HashMap<String, Object> newModel = new HashMap<>(1);
             modelAndView.setModel(newModel);
             return newModel;
         });
         try {
-            viewModelConsumer.accept(request, viewModel);
+            populateModel(request, viewModel);
         } catch (UnsupportedOperationException ex) {
             final HashMap<String, Object> modifiableModel = new HashMap<>(viewModel);
             populateModel(request, modifiableModel);
