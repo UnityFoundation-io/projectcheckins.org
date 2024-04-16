91acf6e60cc6781f11b1b6c588b86afbec6050e0
Sergio del Amo <sergio.delamo@softamo.com>
2024-04-16 13:01:57 +0200

    Update security/src/main/java/org/projectcheckins/security/TeamInvitation.java
    Co-authored-by: Guillermo Calvo <guillermo@guillermo.dev>

--- security/src/main/java/org/projectcheckins/security/TeamInvitation.java
+++ security/src/main/java/org/projectcheckins/security/TeamInvitation.java
@@ -4,6 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public interface TeamInvitation {
+    @Nullable
+    Tenant tenant();
+
     @Email
     @NotBlank
     String email();
