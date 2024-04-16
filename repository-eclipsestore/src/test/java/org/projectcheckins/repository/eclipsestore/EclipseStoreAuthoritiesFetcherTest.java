6e160480d82bf765a47f50920d99ef06e859a42f
Sergio del Amo <sergio.delamo@softamo.com>
2024-04-16 13:03:29 +0200

    Update repository-eclipsestore/src/test/java/org/projectcheckins/repository/eclipsestore/EclipseStoreAuthoritiesFetcherTest.java
    Co-authored-by: Guillermo Calvo <guillermo@guillermo.dev>

--- repository-eclipsestore/src/test/java/org/projectcheckins/repository/eclipsestore/EclipseStoreAuthoritiesFetcherTest.java
+++ repository-eclipsestore/src/test/java/org/projectcheckins/repository/eclipsestore/EclipseStoreAuthoritiesFetcherTest.java
@@ -21,7 +21,7 @@ class EclipseStoreAuthoritiesFetcherTest {
     void authoritiesFetcher(AuthoritiesFetcher authoritiesFetcher, EclipseStoreUser registerService) throws RegistrationCheckViolationException {
         assertThat(authoritiesFetcher.findAuthoritiesByEmail(NOT_FOUND_EMAIL))
             .isEmpty();
-        registerService.register(new UserSave(FOUND_EMAIL, "password", Collections.emptyList()), null);
+        registerService.register(new UserSave(FOUND_EMAIL, "password", null, Collections.emptyList()));
         assertThat(authoritiesFetcher.findAuthoritiesByEmail(FOUND_EMAIL))
             .isEmpty();
         registerService.register(new UserSave(FOUND_EMAIL_WITH_ROLES, "password", Collections.singletonList("ROLE_USER")), null);
