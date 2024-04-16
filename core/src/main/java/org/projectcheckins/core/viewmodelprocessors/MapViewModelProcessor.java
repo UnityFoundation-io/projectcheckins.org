7824004bb00e4e1c9434ad50a2fa449252c44a34
Guillermo Calvo <guillermo@guillermo.dev>
2024-04-16 15:28:00 +0200

    fix: `MapViewModelProcessor` error

--- core/src/main/java/org/projectcheckins/core/viewmodelprocessors/MapViewModelProcessor.java
+++ core/src/main/java/org/projectcheckins/core/viewmodelprocessors/MapViewModelProcessor.java
@@ -7,28 +7,28 @@ import io.micronaut.views.model.ViewModelProcessor;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
 
 public abstract class MapViewModelProcessor implements ViewModelProcessor<Map<String, Object>> {
     @Override
     public void process(@NonNull HttpRequest<?> request, @NonNull ModelAndView<Map<String, Object>> modelAndView) {
-        populateModelAndView(modelAndView, this::populateModel);
+        populateModelAndView(request, modelAndView, this::populateModel);
     }
 
-    protected void populateModelAndView(ModelAndView<Map<String, Object>> modelAndView, Consumer<Map<String, Object>> viewModelConsumer) {
+    private void populateModelAndView(@NonNull HttpRequest<?> request, ModelAndView<Map<String, Object>> modelAndView, BiConsumer<HttpRequest<?>, Map<String, Object>> viewModelConsumer) {
         Map<String, Object> viewModel = modelAndView.getModel().orElseGet(() -> {
             final HashMap<String, Object> newModel = new HashMap<>(1);
             modelAndView.setModel(newModel);
             return newModel;
         });
         try {
-            viewModelConsumer.accept(viewModel);
+            viewModelConsumer.accept(request, viewModel);
         } catch (UnsupportedOperationException ex) {
             final HashMap<String, Object> modifiableModel = new HashMap<>(viewModel);
-            populateModel(modifiableModel);
+            populateModel(request, modifiableModel);
             modelAndView.setModel(modifiableModel);
         }
     }
 
-    protected void populateModel(Map<String, Object> viewModel) {}
+    protected void populateModel(@NonNull HttpRequest<?> request, @NonNull Map<String, Object> viewModel) {}
 }
