c8d11144c0432a2f2b2f0b207e1827241540229f
Guillermo Calvo <guillermo@guillermo.dev>
2024-04-22 10:09:52 +0200

    Use "Project Check-ins" instead of "Check-ins".

--- email/src/main/java/org/projectcheckins/email/EmailInvitationComposerImpl.java
+++ email/src/main/java/org/projectcheckins/email/EmailInvitationComposerImpl.java
@@ -28,7 +28,7 @@ class EmailInvitationComposerImpl implements EmailInvitationComposer {
                                                 @NonNull @NotNull @Valid EmailInvitation emailInvitation) {
         String subject = messageSource.getMessage("email.invitation.subject", "You have been invited to join a Project Check-ins team", locale);
         String callToAction = messageSource.getMessage("email.invitation.calltoaction", "Please, click the link below and signup:", locale);
-        String callToActionLink = messageSource.getMessage("email.invitation.calltoaction.link", "Sign up on Check-ins", locale);
+        String callToActionLink = messageSource.getMessage("email.invitation.calltoaction.link", "Sign up on Project Check-ins", locale);
         String url = emailInvitation.url();
         String text = String.join("\n", callToAction, url);
         String html = String.join("\n", callToAction, "<a href=\"" + url + "\">" + callToActionLink + "</a>");
